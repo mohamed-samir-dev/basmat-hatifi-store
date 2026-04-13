d7d97057ecc1eec62db2753b7da3978256939acb
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-04-13 15:10:48 +0200

    Add

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -35,6 +35,7 @@ export default function ProductCard({ product, priority = false }: { product: Pr
     setTimeout(() => {
       setToast(false);
       setAdded(false);
+      window.scrollTo(0, 0);
       router.push("/cart");
     }, 1000);
   };
