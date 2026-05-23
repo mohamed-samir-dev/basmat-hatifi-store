345b924de81ade53ac5320e4bed0ba5a6c9fea77
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-05-23 03:59:22 +0300

    first

--- app/lib/categoryBanners.ts
+++ app/lib/categoryBanners.ts
@@ -5,13 +5,13 @@ export const categoryBanners: Record<string, string> = {
   // Add more banners here:
    "iphone-17-pro": "/iPhone_17_Pro_Max_Cosmic_Orange_PDP_Image_Position_7__en-ME-scaled (1).avif",
    "iphone-17-air": "/iPhone_Air_Cloud_White_PDP_Image_Position_7__en-ME-scaled(1).avif",
-   "iphone-17":"https://res.cloudinary.com/dyqkhcuxe/image/upload/v1776894316/iPhone_16_Black_PDP_Image_Position_5__en-ME-scaled_o6hv98.avif",
-   "iphone-16-pro-max":"https://res.cloudinary.com/dyqkhcuxe/image/upload/v1776817319/iPhone_16_Pro_Desert_Titanium_PDP_Image_Position_5__en-ME-scaled_h1zf9u.avif",
-   "iphone-16-pro":"https://res.cloudinary.com/dyqkhcuxe/image/upload/v1776817319/iPhone_16_Pro_Desert_Titanium_PDP_Image_Position_5__en-ME-scaled_h1zf9u.avif",
-   "iphone-16-plus":"https://res.cloudinary.com/dyqkhcuxe/image/upload/v1776966167/iPhone_16_Plus_White_PDP_Image_Position_5__en-ME-scaled_bsu8oc.avif",
-   "iphone-16":"https://res.cloudinary.com/dyqkhcuxe/image/upload/v1776966167/iPhone_16_Plus_White_PDP_Image_Position_5__en-ME-scaled_bsu8oc.avif",
-   "iphone-15-pro-max":"https://res.cloudinary.com/dyqkhcuxe/image/upload/v1776966544/iphone-15-pro-max_e0vphb.avif",
-"iphone-15-plus":"https://res.cloudinary.com/dyqkhcuxe/image/upload/v1776966676/iphone_15_plus_hero_cf2xnf.avif",
+   "iphone-17":"https://res.cloudinary.com/dv6fig2ci/image/upload/v1779497909/iPhone_16_Black_PDP_Image_Position_5__en-ME-scaled_o6hv98_wlr9iv.avif",
+   "iphone-16-pro-max":"https://res.cloudinary.com/dv6fig2ci/image/upload/v1779497795/iPhone_16_Pro_Desert_Titanium_PDP_Image_Position_5__en-ME-scaled_h1zf9u_mvmggs.avif",
+   "iphone-16-pro":"https://res.cloudinary.com/dv6fig2ci/image/upload/v1779497795/iPhone_16_Pro_Desert_Titanium_PDP_Image_Position_5__en-ME-scaled_h1zf9u_mvmggs.avif",
+   "iphone-16-plus":"https://res.cloudinary.com/dv6fig2ci/image/upload/v1779497909/iPhone_16_Black_PDP_Image_Position_5__en-ME-scaled_o6hv98_wlr9iv.avif",
+   "iphone-16":"https://res.cloudinary.com/dv6fig2ci/image/upload/v1779497909/iPhone_16_Black_PDP_Image_Position_5__en-ME-scaled_o6hv98_wlr9iv.avif",
+   "iphone-15-pro-max":"https://res.cloudinary.com/dv6fig2ci/image/upload/v1779497806/iphone-15-pro-max_ecyzxn.png",
+"iphone-15-plus":"https://res.cloudinary.com/dv6fig2ci/image/upload/v1779497908/iphone_15_plus_hero_cf2xnf_d8zqmt.avif",
    };
 
 
